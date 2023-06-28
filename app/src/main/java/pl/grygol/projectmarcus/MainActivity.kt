f78afa985ac5f07b0f0fae4c5502a53a8f4a9547
grygol <migryg@gmail.com>
2023-06-28 21:00:17 +0200

    Initial commit

---
+++ app/src/main/java/pl/grygol/projectmarcus/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.grygol.projectmarcus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
