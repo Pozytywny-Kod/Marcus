3a26e67436096635bf2a561fcc131dec9e339c3d
Franek Osiak <franek.osiak123@gmail.com>
2023-06-29 13:20:57 +0200

    Drawer Menu with Expandable List

---
+++ app/src/main/java/pl/grygol/projectmarcus/Adapters/ExpandableListAdapter.kt
@@ -0,0 +1,72 @@
+package pl.grygol.projectmarcus.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.TextView
+import pl.grygol.projectmarcus.R
+
+class ExpandableListAdapter(private val context: Context, private val expandableListData: LinkedHashMap<String, List<String>>) :
+    BaseExpandableListAdapter() {
+
+    override fun getGroupCount(): Int {
+        return expandableListData.size
+    }
+
+    override fun getChildrenCount(groupPosition: Int): Int {
+        val groupTitle = expandableListData.keys.toList()[groupPosition]
+        return expandableListData[groupTitle]?.size ?: 0
+    }
+
+    override fun getGroup(groupPosition: Int): Any {
+        return expandableListData.keys.toList()[groupPosition]
+    }
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Any {
+        val groupTitle = expandableListData.keys.toList()[groupPosition]
+        return expandableListData[groupTitle]?.get(childPosition) ?: ""
+    }
+
+    override fun getGroupId(groupPosition: Int): Long {
+        return groupPosition.toLong()
+    }
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
+    }
+
+    override fun hasStableIds(): Boolean {
+        return true
+    }
+
+    override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup): View {
+        val groupTitle = getGroup(groupPosition) as String
+
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = convertView ?: inflater.inflate(R.layout.list_group, null)
+
+        val titleTextView = view.findViewById<TextView>(R.id.listTitle)
+        titleTextView.text = groupTitle
+
+        return view
+    }
+
+    override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup): View {
+        val groupTitle = getGroup(groupPosition) as String
+        val childTitle = getChild(groupPosition, childPosition) as String
+
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = convertView ?: inflater.inflate(R.layout.list_item, null)
+
+        val titleTextView = view.findViewById<TextView>(R.id.expandableListItem)
+        titleTextView.text = childTitle
+
+        return view
+    }
+
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
+        return true
+    }
+}
--- app/src/main/java/pl/grygol/projectmarcus/MainActivity.kt
+++ app/src/main/java/pl/grygol/projectmarcus/MainActivity.kt
@@ -1,32 +1,75 @@
 package pl.grygol.projectmarcus
 
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ui.AppBarConfiguration
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.navigateUp
-import com.google.android.material.navigation.NavigationView
+import pl.grygol.projectmarcus.Adapters.ExpandableListAdapter
 import pl.grygol.projectmarcus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var expandableListAdapter: ExpandableListAdapter
+    private lateinit var header: View
+    private lateinit var footer: View
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.appBarMain.toolbar)
+        expandableListAdapter = ExpandableListAdapter(this, prepareExpandableListData())
+
+        header = LayoutInflater.from(this).inflate(R.layout.nav_header, binding.drawerLayout, false)
+
+        binding.expandableListView.addHeaderView(header)
+        binding.expandableListView.setAdapter(expandableListAdapter)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
     }
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        return super.onOptionsItemSelected(item)
+    }
+    private fun prepareExpandableListData(): LinkedHashMap<String, List<String>> {
+        val listData = LinkedHashMap<String, List<String>>()
+
+        val group1 = "Group 1"
+        val group2 = "Group 2"
+        val group3 = "Group 3"
+
+        val group1Items = listOf("Item 1", "Item 2", "Item 3")
+        val group2Items = listOf("Item 4", "Item 5", "Item 6")
+        val group3Items = listOf("Item 7", "Item 8", "Item 9")
+
+        listData[group1] = group1Items
+        listData[group2] = group2Items
+        listData[group3] = group3Items
+
+        return listData
     }
 }
