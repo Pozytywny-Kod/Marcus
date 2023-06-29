78e3e1533b065c164f83b3bdffa5ec01d91b0ed0
Franek Osiak <franek.osiak123@gmail.com>
2023-06-29 09:40:12 +0200

    Add Simple Side Navigation Menu with Header

--- app/src/main/java/pl/grygol/projectmarcus/MainActivity.kt
+++ app/src/main/java/pl/grygol/projectmarcus/MainActivity.kt
@@ -1,11 +1,32 @@
 package pl.grygol.projectmarcus
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
 import android.os.Bundle
+import android.view.Menu
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.navigateUp
+import com.google.android.material.navigation.NavigationView
+import pl.grygol.projectmarcus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
     }
 }
