402080e2ad784c9f10b9a4836a091349e01f947a
niani0 <franek.osiak123@gmail.com>
2023-06-30 14:51:20 +0200

    Added date picker

--- app/src/main/java/pl/grygol/projectmarcus/fragments/NewExpenseFormFragment.kt
+++ app/src/main/java/pl/grygol/projectmarcus/fragments/NewExpenseFormFragment.kt
@@ -1,5 +1,8 @@
 package pl.grygol.projectmarcus.fragments
 
+import android.app.DatePickerDialog
+import android.content.Context
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -31,6 +34,25 @@ class NewExpenseFormFragment : Fragment() {
     }
 
     private fun setupViews() {
+        binding.textInputDateLayout.setEndIconOnClickListener {
+            val context = requireContext()
+            openCalendarPicker(context)
+        }
+    }
+
+    private fun openCalendarPicker(context: Context) {
+        val currentDate = Calendar.getInstance()
+        val year = currentDate.get(Calendar.YEAR)
+        val month = currentDate.get(Calendar.MONTH)
+        val day = currentDate.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(context,
+            { _, selectedYear, selectedMonth, selectedDay ->
+                val formattedDate = String.format("%02d/%02d/%04d", selectedDay, selectedMonth + 1, selectedYear)
+                binding.textInputDateEditText.setText(formattedDate)
+            }, year, month, day)
+
+        datePickerDialog.show()
     }
 
 }
